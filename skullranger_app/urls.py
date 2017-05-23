36d378beb0aac25e1494c78b5acb8c12202ed643
Rhea Parekh <rheaparekh12@gmail.com>
2017-05-23 20:56:21 +0530

    added forms and views

--- skullranger_app/urls.py
+++ skullranger_app/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-
+from django.contrib import admin
 from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url(r'^login$',views.login_view,name='login'),
+    url(r'^logout$',views.logout_view,name='logout'),
+    url(r'^signup$',views.signup,name='signup'),
 ]
--- skullranger_app/views.py
+++ skullranger_app/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from skullranger_app.forms import UserCreateForm, AuthenticateForm
+from skullranger_app.models import Post
 from django.http import HttpResponse
 
-def index(request):
-   return HttpResponse("hi")
+def index(request,auth_form=None,user_form=None):
+  if request.user.is_authenticated():
+         return HttpResponse("Logged in")
+  else:
+         auth_form=auth_form or AuthenticateForm()
+         user_form=user_form or UserCreateForm()
+         return render(request,'home.html',{'auth_form':auth_form,'user_form':user_form})
+
+def login_view(request):
+     if request.method=="POST":
+         form = AuthenticateForm(data=request.POST)
+         if form.is_valid():
+               login(reuqest,form.get_user())
+               return redirect('/')
+         else:
+               return index(request,auth_form=form)
+     return redirect('/')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+ 
+def signup(request):
+     user_form=UserCreateForm(data=request.POST)
+     if request.method =='POST':
+        if user_form.is_valid():
+            username=user_form.clean_username()
+            password=user_form.clean_passwordConfirmation
+            user_form.save()
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('/')
+        else:
+            return index(request,user_form=user_form)
+     return redirect('/')
